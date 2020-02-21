cd114ffba4274056546923e34c928cd4800f4f1e
AnastasiaBelova1999 <43291963+AnastasiaBelova1999@users.noreply.github.com>
2020-02-21 20:42:10 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import json
+import hashlib
 
 import mysql
-from flask import Flask, render_template, request, abort, redirect, flash, url_for
+import flask_login
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_login import login_required
-from collections import namedtuple
+import mysql.connector
 
 from mysql_db import MySQL
-import mysql.connector
-import flask_login
-import hashlib
 
 app = Flask(__name__)
 app.secret_key = 'asjdfbajSLDFBhjasbfd'
+app.debug = True
 app.config.from_pyfile('config.py')
 db = MySQL(app)
 login_manager = flask_login.LoginManager()
@@ -21,17 +20,15 @@ login_manager.init_app(app)
 class User(flask_login.UserMixin):
     pass
 
-#Ищем_в_бд_чтобы_логин_и_пароль_совпадали_правильно
 @login_manager.user_loader
 def user_loader(login):
     cursor = db.db.cursor(named_tuple=True)
-    cursor.execute('select id, login, name from users where id = %s', (login,))
+    cursor.execute('select id, login from users_exam where id = %s', (login,))
     user_db = cursor.fetchone()
     if user_db:
         user = User()
         user.id = user_db.id
         user.login = user_db.login
-        user.name = user_db.name
         return user
     return None
 
@@ -40,8 +37,7 @@ def user_loader(login):
 def unauthorized_handler():
     return render_template("index.html", authorization=False, login="anonimus", login_false=False)
 
-#[get]-получить_у_сервева,_[post] - отдать_серверу
-#формочка, где вход пользователя
+
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'GET':
@@ -59,7 +55,7 @@ def hello_world():
             cursor = db.db.cursor(named_tuple=True, buffered=True)
             try:
                 cursor.execute(
-                    "SELECT id,name,login FROM users WHERE `login` = '%s' and `password_hash` = '%s'" % (
+                    "SELECT id,login FROM users_exam WHERE `login` = '%s' and `password_hash` = '%s'" % (
                         username, password_hash))
                 user = cursor.fetchone()
             except Exception:
@@ -71,7 +67,6 @@ def hello_world():
                 flask_user = User()
                 flask_user.id = user.id
                 flask_user.login = user.login
-                flask_user.name = user.name
                 flask_login.login_user(flask_user, remember=True)
                 return render_template("index.html", authorization=not flask_login.current_user.is_anonymous,
                                        login=user.login, login_false=False)
@@ -85,15 +80,6 @@ def hello_world():
                                    login="anonimus", login_false=True)
 
 
-@app.route('/main', methods=['POST', 'GET'])
-def main():
-    user = flask_login.current_user
-    if not user.is_anonymous:
-        return user.name
-    else:
-        return "Залогинтесь"
-
-
 @app.route('/logout', methods=['GET'])
 def logout():
     flask_login.logout_user()
@@ -101,188 +87,110 @@ def logout():
                            login_false=False)
 
 
-@app.route('/town', methods=['GET'])
+@app.route('/list', methods=['GET'])
 @login_required
-def town():
-    towns = db.select(None, "towns")
+def list():
+    lists = db.select(None, "lists")
     login = flask_login.current_user.login
-    return render_template("town.html", towns=towns, login=login)
+    types = dict(db.select(None, "types"))
+    status = dict(db.select(["id", "title"], "status"))
+    return render_template("call-list.html", login=login, lists=lists, types=types, status=status)
 
 
-@app.route('/tariff', methods=['GET'])
+@app.route('/list/delete', methods=['POST'])
 @login_required
-def tariff():
-    tariffs = db.select(None, "tariffs")
-    login = flask_login.current_user.login
-    return render_template("tariff.html", tariffs=tariffs, login=login)
-
-
-@app.route('/tariff/new', methods=['GET', 'POST'])
-@login_required
-def tariff_new():
-    if request.method == 'GET':
-        return render_template("tarrif_new.html", login=flask_login.current_user.login)
-    elif request.method == 'POST':
-        id = request.form.get("id")
-        title = request.form.get("title")
-        price = request.form.get("price")
-        description = request.form.get("description")
-        review = request.form.get("review")
-        if id and title and price and description:
-            cursor = db.db.cursor(named_tuple=True)
-            try:
-                cursor.execute(
-                    "INSERT INTO `tariffs` (`id`, `title`, `price`, `description`, `review`) VALUES ('%s', '%s', '%s', '%s','%s')" % (
-                        id, title, price, description, review))
-                db.db.commit()
-                cursor.close()
-                return redirect("/tariff")
-            except Exception:
-                return render_template("tarrif_new.html", login=flask_login.current_user.login, insert_false=True)
-
-
-@app.route('/tariff/edit', methods=['POST'])
-@login_required
-def tariff_edit():
-    try:
-        tariff_id = request.form.get("tariff_id")
-        title = request.form.get("title")
-        price = request.form.get("price")
-        description = request.form.get("description")
-        review = request.form.get("review")
-        tariff = {
-            'id': tariff_id,
-            'title': title,
-            'price': price,
-            'description': description,
-            'review': review
-        }
-        return render_template("tariff_edit.html", tariff=tariff)
-    except Exception:
-        return redirect("/tariff")
-
-
-@app.route('/tariff/edit/submit', methods=['POST'])
-@login_required
-def tariff_edit_submit():
-    older_id = request.form.get("older_id")
+def list_delete():
     id = request.form.get("id")
-    title = request.form.get("title")
-    price = request.form.get("price")
-    description = request.form.get("description")
-    review = request.form.get("review")
-    if id and title and price and description:
+    cursor = db.db.cursor()
+    cursor.execute("DELETE FROM `lists` WHERE `lists`.`id` = '%s'" % id)
+    db.db.commit()
+    cursor.close()
+    return redirect("/list")
+
+
+@app.route('/list/edit', methods=['POST'])
+@login_required
+def list_edit():
+    try:
+        list_id = request.form.get("list_id")
+        date = request.form.get("data")
+        login = request.form.get("login_users")
+        type_id = request.form.get("type_id")
+        status_id = request.form.get("status_id")
+        message = request.form.get("message")
+        statuss = db.select(None, "status")
+        types = db.select(None, "types")
+        sub = {
+            'list_id': list_id,
+            'date': date,
+            'login_users': login,
+            'type_id': type_id,
+            'status_id': status_id,
+            'message': message
+        }
+        return render_template("list_edit.html", sub=sub, types=types, statuss=statuss,
+                               login=flask_login.current_user.login, user_id=flask_login.current_user.id
+                               , user_role=1, list_id=list_id)
+
+    except Exception:
+        return redirect(url_for("list"))
+
+
+@app.route('/list/edit/submit', methods=['POST'])
+@login_required
+def sub_edit_submit():
+    list_id = request.form.get("id")
+    date = request.form.get("data")
+    type_id = request.form.get("type_id")
+    status_id = request.form.get("status_id")
+    message = request.form.get("message")
+
+    if date and status_id and type_id:
         cursor = db.db.cursor(named_tuple=True)
         try:
             cursor.execute(
-                "UPDATE `tariffs` SET `id` = '%s', `title` = '%s', `price` = '%s', `description` = '%s', `review` = '%s' WHERE `tariffs`.`id` = %s" % (
-                    id, title, price, description, review, older_id))
+                "UPDATE `lists` SET  `data` = '%s', `status_id` = '%s', `type_id` = '%s',`message` = '%s' WHERE `lists`.`id` = '%s'" % (
+                    date, status_id, type_id, message, list_id))
             db.db.commit()
             cursor.close()
-            return redirect("/tariff")
+            return redirect("/list")
         except Exception:
-            return render_template("tarrif_new.html", login=flask_login.current_user.login, insert_false=True)
+            return redirect("/list")
+    else:
+        return redirect("/list")
 
 
-@app.route('/sub', methods=['GET'])
-@login_required
-def sub():
-    subs = db.select(None, "subscribers")
-    towns = dict(db.select(None, "towns"))
-    tariffs = dict(db.select(["id", "title"], "tariffs"))
-
-    view_subs = []
-    for sub in subs:
-        view_subs.append({"id": sub.id,
-                          "full_name": sub.full_name,
-                          "town": towns[sub.town_id],
-                          "tariff": tariffs[sub.tariff_id],
-                          "town_id": sub.town_id,
-                          "tariff_id": sub.tariff_id
-                          })
-    return render_template("sub.html", login=flask_login.current_user.login, subs=view_subs)
-
-
-@app.route('/sub/delete', methods=['POST'])
-@login_required
-def sub_delete():
-    id = request.form.get("id")
-    cursor = db.db.cursor()
-    cursor.execute("DELETE FROM `subscribers` WHERE `subscribers`.`id` = '%s'" % id)
-    cursor.close()
-    return redirect("/sub")
-
-
-@app.route('/sub/new', methods=['POST', 'GET'])
+@app.route('/list/new', methods=['POST', 'GET'])
 @login_required
 def sub_new():
     if request.method == 'GET':
-        towns = db.select(["id", "title"], "towns")
-        tariffs = db.select(["id", "title"], "tariffs")
-        return render_template("sub_new.html", towns=towns, tariffs=tariffs)
+        types = db.select(["id", "title"], "types")
+        status = db.select(["id", "title"], "status")
+        return render_template("new.html", types=types, status=status)
     elif request.method == 'POST':
-        id = request.form.get("id")
-        full_name = request.form.get("full_name")
-        town_id = request.form.get("town")
-        tariff_id = request.form.get("tariff")
-        if id and full_name and town_id and tariff_id:
+        data = request.form.get("data")
+        login_users = request.form.get("login_users")
+        type_id = request.form.get("type_id")
+        status_id = request.form.get("status_id")
+        message = request.form.get("message")
+        if data and login_users and type_id and status_id and message:
             cursor = db.db.cursor(named_tuple=True)
             try:
                 cursor.execute(
-                    "INSERT INTO `subscribers` (`id`, `full_name`, `town_id`, `tariff_id`) VALUES ('%s', '%s', '%s', '%s')" % (
-                        id, full_name, town_id, tariff_id))
-                db.db.commit()
+                    "INSERT INTO `lists` (`data`, `login_users`, `type_id`,`status_id`, `message`) VALUES ( '%s', '%s', '%s','%s','%s')" % (
+                        data, login_users, type_id, status_id, message))
                 cursor.close()
-                return redirect("/sub")
+                return redirect("/list")
             except Exception:
-                return render_template("sub_new.html", login=flask_login.current_user.login, insert_false=True)
+                types = db.select(["id", "title"], "types")
+                status = db.select(["id", "title"], "status")
+                return render_template("new.html", login=flask_login.current_user.login, insert_false=True, types=types,
+                                       status=status)
         else:
-            return render_template("sub_new.html", login=flask_login.current_user.login, insert_false=True)
-
-
-@app.route('/sub/edit', methods=['POST'])
-@login_required
-def sub_edit():
-    try:
-        sub_id = request.form.get("id")
-        full_name = request.form.get("full_name")
-        town_id = request.form.get("town_id")
-        tariff_id = request.form.get("tariff_id")
-        towns = db.select(["id", "title"], "towns")
-        tariffs = db.select(["id", "title"], "tariffs")
-        sub = {
-            'id': sub_id,
-            'full_name': full_name,
-            'town_id': town_id,
-            'tariff_id': tariff_id
-        }
-        return render_template("sub_edit.html", sub=sub, towns=towns, tariffs=tariffs,
-                               login=flask_login.current_user.login)
-    except Exception:
-        return redirect("/sub")
-
-@app.route('/sub/edit/submit', methods=['POST'])
-@login_required
-def sub_edit_submit():
-    older_id = request.form.get("older_id")
-    sub_id = request.form.get("id")
-    full_name = request.form.get("full_name")
-    town_id = request.form.get("town_id")
-    tariff_id = request.form.get("tariff_id")
-    if sub_id and full_name and town_id and tariff_id:
-        cursor = db.db.cursor(named_tuple=True)
-        try:
-            cursor.execute(
-                "UPDATE `subscribers` SET `id` = '%s', `full_name` = '%s', `town_id` = '%s', `tariff_id` = '%s' WHERE `subscribers`.`id` = %s" % (
-                    sub_id, full_name, town_id, tariff_id, older_id))
-            db.db.commit()
-            cursor.close()
-            return redirect("/sub")
-        except Exception:
-            return redirect("/sub")
-    else:
-        return redirect("/sub")
-
+            types = db.select(["id", "title"], "types")
+            status = db.select(["id", "title"], "status")
+            return render_template("new.html", login=flask_login.current_user.login, insert_false=True, types=types,
+                                   status=status)
 
 
 if __name__ == '__main__':
